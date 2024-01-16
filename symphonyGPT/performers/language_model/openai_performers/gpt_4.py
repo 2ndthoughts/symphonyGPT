996875dd454f1620f5edada186a06870c9c90e06
David Loo <david@2ndthoughts.app>
2024-01-16 14:32:14 -0600

    gpt-4-1106-preview now allows 128K tokens

--- symphonyGPT/performers/language_model/openai_performers/gpt_4.py
+++ symphonyGPT/performers/language_model/openai_performers/gpt_4.py
@@ -5,7 +5,7 @@ class Gpt4(ChatCompletionPerformer):
 
     def __init__(self):
         super().__init__()
-        self.set_model_attribute("model", "gpt-4")
+        self.set_model_attribute("model", "gpt-4-1106-preview")
 
     def perform(self, prompt):
         return super().perform(prompt)
