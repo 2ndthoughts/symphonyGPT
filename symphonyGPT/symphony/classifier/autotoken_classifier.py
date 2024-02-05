5799d06307e95c5f08b972b0ac4dcc1be0a20a53
David Loo <david@2ndthoughts.app>
2024-02-05 02:12:15 -0800

    add ability to load tensorflow model bins

--- symphonyGPT/symphony/classifier/autotoken_classifier.py
+++ symphonyGPT/symphony/classifier/autotoken_classifier.py
@@ -4,13 +4,14 @@ from symphonyGPT.symphony.classifier.classifier import Classifier
 
 class AutoTokenClassifier(Classifier):
 
-    def __init__(self, classification_type_str=None):
+    def __init__(self, classification_type_str=None, from_tf=False):
         super().__init__(classification_type_str)
-        self.classification_type_str = "token-classification" # default to text classifier
+        self.classification_type_str = "token-classification"  # default to text classifier
+        self.from_tf = from_tf
 
     def _classify(self, classifier_model, prompt):
         tokenizer = AutoTokenizer.from_pretrained(self.model)
-        model = AutoModelForTokenClassification.from_pretrained(self.model)
+        model = AutoModelForTokenClassification.from_pretrained(self.model, from_tf=self.from_tf)
 
         pipe = pipeline("token-classification", model=model, tokenizer=tokenizer,
                         aggregation_strategy="simple")  # pass device=0 if using gpu
