97dc25ebdd4a0d41e58475dfb6f6b03d21eb7c91
David Loo <david@2ndthoughts.app>
2025-07-22 16:58:09 -0500

    caching prompts and responses, cache removed at shutdown

--- symphonyGPT/symphony/symphony_cache.py
+++ symphonyGPT/symphony/symphony_cache.py
@@ -7,10 +7,47 @@ from diskcache import Cache
 from symphonyGPT.symphony.util import Util
 
 
+def delete_old_cache_dirs():
+    import os
+    import shutil
+    import time
+
+    # Define the directory where the folders are located
+    directory_path = '/tmp'
+
+    # Define the prefix of the folders you want to delete
+    folder_prefix = 'symphonyGPT_cache'
+
+    # Define the age limit (in seconds)
+    age_limit_seconds = 1 * 24 * 60 * 60  # 1 day in seconds
+
+    # Get the current time in seconds since the epoch
+    current_time = time.time()
+
+    # List all the items in the directory
+    for item in os.listdir(directory_path):
+        item_path = os.path.join(directory_path, item)
+
+        # Check if the item is a directory
+        if os.path.isdir(item_path) and item.startswith(folder_prefix):
+            # Get the creation time of the folder
+            creation_time = os.path.getctime(item_path)
+
+            # Calculate the age of the folder
+            folder_age = current_time - creation_time
+
+            # Check if the folder is older than the age limit
+            if folder_age > age_limit_seconds:
+                # Delete the folder if it's older than 1 day
+                shutil.rmtree(item_path)
+                Util().debug_print(f"delete_old_cache_dirs deleted: {item_path}")
+
+
 class SymphonyCache:
     def __init__(self, cache_dir=None):
         if cache_dir is not None:
-            self.cache_dir = cache_dir + "." + str(os.getpid())
+            # self.cache_dir = cache_dir + "." + str(os.getpid())
+            self.cache_dir = cache_dir
         else:
             # Create a temporary directory for the cache
             self.cache_dir = tempfile.mkdtemp()
@@ -18,41 +55,6 @@ class SymphonyCache:
         self.cache = Cache(self.cache_dir)
         # Register the cleanup function to run on process exit
         atexit.register(self.cleanup_cache_dir)
-
-    def delete_old_cache_dirs(self):
-        import os
-        import shutil
-        import time
-
-        # Define the directory where the folders are located
-        directory_path = '/tmp'
-
-        # Define the prefix of the folders you want to delete
-        folder_prefix = 'symphonyGPT_cache'
-
-        # Define the age limit (in seconds)
-        age_limit_seconds = 1 * 24 * 60 * 60  # 1 day in seconds
-
-        # Get the current time in seconds since the epoch
-        current_time = time.time()
-
-        # List all the items in the directory
-        for item in os.listdir(directory_path):
-            item_path = os.path.join(directory_path, item)
-
-            # Check if the item is a directory
-            if os.path.isdir(item_path) and item.startswith(folder_prefix):
-                # Get the creation time of the folder
-                creation_time = os.path.getctime(item_path)
-
-                # Calculate the age of the folder
-                folder_age = current_time - creation_time
-
-                # Check if the folder is older than the age limit
-                if folder_age > age_limit_seconds:
-                    # Delete the folder if it's older than 1 day
-                    shutil.rmtree(item_path)
-                    Util().debug_print(f"delete_old_cache_dirs deleted: {item_path}")
 
     # Function to cleanup the cache directory
     def cleanup_cache_dir(self):
